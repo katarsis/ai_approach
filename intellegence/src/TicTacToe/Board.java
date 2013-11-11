15c46cad768cd3902df350093392546526dbc4fd
U1326317 <U1326317@mos-wks0437.ceb.loc>
2013-11-11 09:49:57 +0400

    implement state

---
+++ intellegence/src/TicTacToe/Board.java
@@ -0,0 +1,67 @@
+package TicTacToe;
+
+public class Board {
+
+	private int [][] boardsFlag;
+	private final int CROSS_FLAG = 1;
+	private final int ZERO_FLAG =0;
+	private final int NONE_FLAG =-1;
+	private final int DIMENSION_SIZE =3;
+	
+	public Board()
+	{
+		boardsFlag =  new int [DIMENSION_SIZE][DIMENSION_SIZE];
+		for(int i=0; i<DIMENSION_SIZE;i++)
+		{
+			for(int j=0;j<DIMENSION_SIZE;j++)
+			{
+				boardsFlag[i][j]=NONE_FLAG;
+			}
+		}
+	}
+	
+	
+	public boolean isEmptyFeild(int xPosition, int yPosition)
+	{
+		if(boardsFlag[xPosition][yPosition]==NONE_FLAG)
+			return true;
+		return false;
+	}
+	
+	public void setCrossAt(int xPosition, int yPosition)
+	{
+		boardsFlag[xPosition][yPosition]=CROSS_FLAG;
+	}
+	
+	public void setZeroAt(int xPosition,int yPosition)
+	{
+		boardsFlag[xPosition][yPosition]=ZERO_FLAG;
+	}
+	
+	public void print()
+	{
+		for(int i=0;i<DIMENSION_SIZE;i++)
+		{
+			String outline="";
+			for(int j=0;j<DIMENSION_SIZE;j++)
+			{
+				outline+=boardsFlag[i][j];
+				outline+="|";
+			}
+			System.out.println(outline);
+			System.out.println("---------------");
+		}
+	}
+
+
+	public int getBoardsFlagAt(int xPos, int yPos) 
+	{
+		return boardsFlag[xPos][yPos];
+	}
+
+
+	/*public void setBoardsFlagAt(int[][] boardsFlag) {
+		this.boardsFlag = boardsFlag;
+	}*/
+}
+
--- intellegence/src/TicTacToe/StateGame.java
+++ intellegence/src/TicTacToe/StateGame.java
@@ -6,15 +6,28 @@ import piton.tree.IState;
 
 public class StateGame implements IState{
 
+	private Board boardState;
+	
 	@Override
 	public boolean IsPossibleState() {
-		// TODO Auto-generated method stub
-		return false;
+		// all states possible then all get from empty feild
+		return true;
 	}
 
 	@Override
 	public boolean IsFinState() {
 		// TODO Auto-generated method stub
+		/*
+		 * First step see all horizontal for watch fin state
+		 * if fin state is find return true
+		 * second step see all verticals for watch fin state
+		 * if fin state is find return true
+		 * Third step see 1-9 diagonal for watch fin state
+		 * if fin state is find return true
+		 * Fourth step see 7-3 diagonal for watch fin state
+		 * if fin state is find return true
+		 * else return false
+		 */
 		return false;
 	}
 
