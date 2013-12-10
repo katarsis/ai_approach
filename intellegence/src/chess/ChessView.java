86c875b07f9333258b7398744a362a1c1b9e2ab9
U1326317 <U1326317@mos-wks0437.ceb.loc>
2013-12-10 11:56:29 +0400

    new

--- intellegence/src/chess/ChessView.java
+++ intellegence/src/chess/ChessView.java
@@ -9,60 +9,45 @@ import javax.swing.JPanel;
 import java.lang.*;
 
 public class ChessView extends JPanel {
+	Image image;
 	
-	private static void doDrawing(Graphics g) {
+	public ChessView(){
+		Dimension size = new Dimension(400,400);
+		setSize(size);
+		setLayout(null);
+	}
+	
+	private void doDrawing(Graphics g) {
 	        
 	        Graphics2D g2d = (Graphics2D) g;
-	        
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            BufferedImage resized = new BufferedImage(200, 300, BufferedImage.TYPE_INT_ARGB_PRE);
+	        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            BufferedImage resized = new BufferedImage(400, 400, BufferedImage.TYPE_INT_ARGB_PRE);
             Graphics2D imageGr = (Graphics2D) resized.createGraphics();
-	       /* float[] dash1 = {2f, 0f, 2f};
-	        float[] dash2 = {1f, 1f, 1f};
-	        float[] dash3 = {4f, 0f, 2f};
-	        float[] dash4 = {4f, 4f, 1f};
-
-	        g2d.drawLine(20, 40, 250, 40);
-
-	        BasicStroke bs1 = new BasicStroke(1, BasicStroke.CAP_BUTT,
-	                BasicStroke.JOIN_ROUND, 1.0f, dash1, 2f);
-
-	        BasicStroke bs2 = new BasicStroke(1, BasicStroke.CAP_BUTT,
-	                BasicStroke.JOIN_ROUND, 1.0f, dash2, 2f);
-
-	        BasicStroke bs3 = new BasicStroke(1, BasicStroke.CAP_BUTT,
-	                BasicStroke.JOIN_ROUND, 1.0f, dash3, 2f);
-
-	        BasicStroke bs4 = new BasicStroke(1, BasicStroke.CAP_BUTT,
-	                BasicStroke.JOIN_ROUND, 1.0f, dash4, 2f);
-
-	        g2d.setStroke(bs1);
-	        g2d.drawLine(20, 80, 250, 80);
-
-	        g2d.setStroke(bs2);
-	        g2d.drawLine(20, 120, 250, 120);
-
-	        g2d.setStroke(bs3);
-	        g2d.drawLine(20, 160, 250, 160);
-
-	        g2d.setStroke(bs4);
-	        g2d.drawLine(20, 200, 250, 200);*/
-	        Toolkit tools =  Toolkit.getDefaultToolkit();
-	        String imageLink = "/img/chessboard.png";
+            imageGr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            
+            Toolkit tools =  Toolkit.getDefaultToolkit();
+	        String imageLink = "img/images.org/chessboard.png";
 	        Image img = null;
+	        
 	        URL url = null;
-	        url = imageLink.getClass().getResource(imageLink);
+	        url = getClass().getResource(imageLink);
+	        
 	        img = tools.getImage(url);
-	        imageGr.drawImage(img, 0, 0, null);
-
+	        imageGr.drawImage(img, 0, 0, 400, 400, null);
+            imageGr.dispose();
+             image = resized.getScaledInstance(400, 400, 0);
+	        //imageGr.drawImage(img, 0, 0, 200,200,null);
+	        g2d.drawImage(image, 0, 0, null);
+	        
+	        
+	  
+	        
     }
-	    
 	 
 	@Override
-	    public void paintComponent(Graphics g) {
-	        
+	public void paintComponent(Graphics g) {
 	        super.paintComponent(g);
 	        doDrawing(g);
-	    }
+	}
 
 }
--- intellegence/src/chess/MainWindow.java
+++ intellegence/src/chess/MainWindow.java
@@ -21,56 +21,33 @@ public class MainWindow extends JFrame {
 	 * @param style
 	 */
 	public MainWindow() {
-		
-		//super(parent, SWT.NONE);
+		super();
 		initUI();
 	}
 	
 	public final void initUI(){
 		ChessView pnl = new ChessView();
-		getContentPane().add(pnl);
-		
-		 setSize(390, 358);
-	        setTitle("Lines");
-	        setLocationRelativeTo(null);
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		Container con =  getContentPane();
+		con.setLayout(null);
+		con.add(pnl);
+		setSize(400, 400);
+        setTitle("Lines");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+	}    
 	
 	
 	 public static void main(String[] args) {
-
-	        SwingUtilities.invokeLater(new Runnable() {
+	       SwingUtilities.invokeLater(new Runnable() {
 	            @Override
 	            public void run() {
 	            	MainWindow ex = new MainWindow();
-	                ex.setVisible(true);
 	            }
 	        });
+
 	    }
-	//@Override
-/*	protected void checkSubclass() {
-		// Disable the check that prevents subclassing of SWT components
-	}*/
+
 	
-	/* public void paint(Graphics g) {
-		    Graphics2D g2 = (Graphics2D)g;
-		    double x = 15, y = 50, w = 70, h = 70;
-		    Ellipse2D e = new Ellipse2D.Double(x, y, w, h);
-		    GradientPaint gp = new GradientPaint(75, 75, Color.white,
-		        95, 95, Color.gray, true);
-		    // Fill with a gradient.
-		    g2.setPaint(gp);
-		    g2.fill(e);
-		    // Stroke with a solid color.
-		    e.setFrame(x + 100, y, w, h);
-		    g2.setPaint(Color.black);
-		    g2.setStroke(new BasicStroke(8));
-		    g2.draw(e);
-		    // Stroke with a gradient.
-		    e.setFrame(x + 200, y, w, h);
-		    g2.setPaint(gp);
-		    g2.draw(e);
-		  }*/
 }
 
 
