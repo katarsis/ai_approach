d0e14ffbed264ee9b12c189d2c905c3cd0b46b5f
U1326317 <U1326317@mos-wks0437.ceb.loc>
2014-02-21 09:29:31 +0400

    ule logic

---
+++ intellegence/src/logic/KB_DAO.java
@@ -0,0 +1,8 @@
+package logic;
+/*
+ * this class represents the simple KB uses sqlLite databases
+ * 
+ */
+public class KB_DAO {
+
+}
---
+++ intellegence/src/logic/simpleSlover.java
@@ -0,0 +1,17 @@
+package logic;
+/*
+ * this class represents the tt-enitals algoritm 
+ * for KB uses sqlLite database 
+ * 
+ */
+public class simpleSlover {
+
+	
+	public static void doSlove() {
+		/*
+		 * while exist the unsed sybols 
+		 * do step 
+		 */
+	}
+	
+}
