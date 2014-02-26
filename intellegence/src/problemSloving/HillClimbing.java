2f4a17409a09a5cc897c93147a53be4b604e7998
U1326317 <U1326317@mos-wks0437.ceb.loc>
2014-02-26 16:16:27 +0400

    add ploter utils

--- intellegence/src/problemSloving/HillClimbing.java
+++ intellegence/src/problemSloving/HillClimbing.java
@@ -1,7 +1,15 @@
 package problemSloving;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
+import org.jfree.ui.RefineryUtilities;
+
+import utils.Plotter;
 import exception.NonInitals;
 
 
@@ -12,6 +20,7 @@ public class HillClimbing {
 		// TODO Auto-generated method stub
 		TaskOfQueens task = new TaskOfQueens(0,0,0,0,0,0,0,0);
 		int iteration = 0 ;
+		ArrayList <String> dataSeries =  new ArrayList();
 		for(int k=0; k<100; k++)
 		{
 			task = new TaskOfQueens(0,0,0,0,0,0,0,0);
@@ -23,11 +32,23 @@ public class HillClimbing {
 							.xPosition, i);
 				task.setQueenAt(goingPoint.xPosition, goingPoint.yPosition);
 				iteration++;
+				
 			}
+			dataSeries.add(String.valueOf(iteration));
 			System.out.println(iteration);
 		}
 		System.out.println(task.toString());
+		Collections.sort(dataSeries,new Comparator<String>() {
+			public int compare(String a, String b)
+			{
+				return Integer.signum(Integer.valueOf(a)-Integer.valueOf(b));
+			}
+		});
+		Object[] ObjectList = dataSeries.toArray();
+		String[]items =  Arrays.copyOf(ObjectList,ObjectList.length,String[].class);
 		
+		  final Plotter demo = new Plotter(items,1500,800);
+	
 	}
 	
 	public static int randRange(int minimum, int maximum)
---
+++ intellegence/src/utils/Plotter.java
@@ -0,0 +1,79 @@
+package utils;
+import java.awt.Color;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.util.ShapeUtilities;
+
+public class Plotter extends ApplicationFrame {
+
+	String [] dataMassive; 
+	
+	public Plotter(String []inpt, int sizeX, int sizeY) {
+		super("plot");
+		final DefaultCategoryDataset dataSet = createDataSet(inpt);
+		final JFreeChart chart = createChart(dataSet);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(sizeX, sizeY));
+        setContentPane(chartPanel);
+        this.pack();
+        RefineryUtilities.centerFrameOnScreen(this);
+        this.setVisible(true);
+	}
+	
+	public DefaultCategoryDataset createDataSet(String[] dataSet)
+	{
+		DefaultCategoryDataset series = new DefaultCategoryDataset();
+		for(int i =0; i<dataSet.length;i++)
+			series.addValue(Double.valueOf(dataSet[i]), "SERIES", String.valueOf(i));
+
+		return series;
+	}
+	
+	public JFreeChart createChart(CategoryDataset dataSet){
+		 final JFreeChart localJFreeChart = ChartFactory.createLineChart(
+		            "Plot",
+		            "X",   
+		            "Y",   
+		            dataSet,
+		            PlotOrientation.VERTICAL,
+		            true,   
+		            true,   
+		            false   
+		        );
+
+			CategoryPlot localCategoryPlot = (CategoryPlot) localJFreeChart
+					.getPlot();
+			NumberAxis localNumberAxis = (NumberAxis) localCategoryPlot
+					.getRangeAxis();
+			localNumberAxis.setStandardTickUnits(NumberAxis
+					.createIntegerTickUnits());
+			LineAndShapeRenderer localLineAndShapeRenderer = (LineAndShapeRenderer) localCategoryPlot
+					.getRenderer();
+			localLineAndShapeRenderer.setSeriesShapesVisible(0, true);
+			localLineAndShapeRenderer.setSeriesShapesVisible(1, false);
+			localLineAndShapeRenderer.setSeriesShapesVisible(2, true);
+			localLineAndShapeRenderer.setSeriesLinesVisible(2, false);
+			localLineAndShapeRenderer.setSeriesShape(2,
+					ShapeUtilities.createDiamond(4.0F));
+			localLineAndShapeRenderer.setDrawOutlines(true);
+			localLineAndShapeRenderer.setUseFillPaint(true);
+			localLineAndShapeRenderer.setBaseFillPaint(Color.white);
+			return localJFreeChart;
+	
+	}
+}
